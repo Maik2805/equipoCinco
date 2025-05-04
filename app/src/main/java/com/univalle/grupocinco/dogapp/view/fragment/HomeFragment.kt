1d368051abb167e82020ff37db622b9cdb05a6f8
mscardenas77@misena.edu.co <mscardenas77@misena.edu.co>
2025-05-04 13:27:18 -0500

    [IMPL] HU 3.0: Ventana Nueva Cita -> Se realiza implementación de diseño

--- app/src/main/java/com/univalle/grupocinco/dogapp/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/univalle/grupocinco/dogapp/view/fragment/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.univalle.grupocinco.dogapp.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.univalle.grupocinco.dogapp.R
 import com.univalle.grupocinco.dogapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,11 +25,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        controllers()
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 activity?.moveTaskToBack(true)
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
+
+    private fun controllers() {
+        binding.btnAddAppointment.setOnClickListener {
+            try {
+                Log.d("NAVIGATION", "Trying to navigate now")
+                findNavController().navigate(R.id.action_appointmentManagerFragment_to_addAppointmentFragment)
+            } catch (e: Exception) {
+                Log.e("NAVIGATION", "Navigation failed", e)
+            }
+        }
     }
 }
